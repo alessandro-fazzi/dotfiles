6ae7534c699a530056372389dd58c66cad6e9cb2
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2015-09-21 15:24:25 +0200

    added oh-my-fish

---
+++ omf/pkg/upbrew/upbrew.fish
@@ -0,0 +1,32 @@
+# SYNOPSIS
+#   upbrew [options]
+#
+# USAGE
+#   Options
+#
+
+function init -a path --on-event init_upbrew
+  if not available brew
+    echo "Please install 'brew' first!"; return 1
+  end
+end
+
+function upbrew -d "Upgrade brew and brew-cask packages and clean 'open with' menu"
+  brew update
+  brew upgrade
+
+  for app in (brew cask list)
+    brew cask install $app $force
+  end
+
+  brew cleanup
+  brew-cask cleanup
+
+  /System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user
+  killall Finder
+  echo "Open With menu has been rebuilt, Finder will relaunch"
+end
+
+function uninstall --on-event uninstall_upbrew
+
+end
