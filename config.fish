cc0d46df396fa48bc70e6a4aca65f2638290503e
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2015-10-08 17:49:31 +0200

    bundler omg plugin and custom config in config.fish

--- config.fish
+++ config.fish
@@ -7,6 +7,9 @@ set -g OMF_CONFIG $HOME/.config/omf
 # Path for z jump around
 set -g Z_SCRIPT_PATH ~/git/z/z.sh
 
+# Adds commands for omf-bundler plugin
+set bundler_plugin_execs gem rails
+
 ### Configuration required to load oh-my-fish ###
 # Note: Only add configurations that are required to be set before oh-my-fish is loaded.
 # For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
