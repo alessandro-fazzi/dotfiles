7b63064f7e014d475f7350c28d3752b065d5b2e9
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2016-02-10 12:53:40 +0100

    upbrew fish package: clean up old casks based on install date

--- omf/pkg/upbrew/upbrew.fish
+++ omf/pkg/upbrew/upbrew.fish
@@ -21,10 +21,26 @@ function upbrew -d "Upgrade brew and brew-cask packages and clean 'open with' me
 
   brew cleanup
   brew cask cleanup
+  upbrew_delete_old_brew_casks
 
   /System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user
   killall Finder
   echo "Open With menu has been rebuilt, Finder will relaunch"
+end
+
+function upbrew_delete_old_brew_casks -d "Delete older-than-the-last-installed-version casks versions"
+  if not test "Darwin" = (uname)
+    return 0
+  end
+
+  cd /opt/homebrew-cask/Caskroom
+
+  for cask in (ls | tr -d '/')
+    for old_cask in (ls -t -1 $cask | tail -n +2 | tr -d '/')
+      echo "Deleting old cask $cask version $old_cask"
+      rm -rf "$cask/$old_cask"
+    end
+  end
 end
 
 function uninstall --on-event uninstall_upbrew
