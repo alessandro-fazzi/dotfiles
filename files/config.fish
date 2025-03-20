0972fb50caf13ae56e55a150cfce945eda3fd63d
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2025-03-20 12:31:27 +0100

    git: gone/prune utils

--- files/config.fish
+++ files/config.fish
@@ -50,6 +50,39 @@ function camar_socks5_proxy -d "Open a SOCKS5 proxy on the local 3128 port, pass
   ssh -f -C -N -D 0.0.0.0:3128 camar-redmine-production
 end
 
+function git-prune-gone -d "Delete local branches that have no remote tracking branch"
+    git fetch -p
+
+    set -l local_branches (git branch -vv | grep ": gone]" | awk '{print $1}')
+
+    if test -z "$local_branches"
+        echo "No branches with gone remotes found."
+        return 0
+    end
+
+    echo "Found branches with gone remotes:"
+
+    for branch in $local_branches
+        # Simpler approach to extract remote info
+        set -l remote_info (git for-each-ref --format='%(upstream:short)' refs/heads/$branch)
+
+        echo -e "\nLocal branch: "(set_color cyan)"$branch"(set_color normal)
+        echo "Remote was: "(set_color red)"$remote_info"(set_color normal)" (gone)"
+
+        read -l -P "Delete this branch? (y/n/q) " choice
+        switch $choice
+            case y Y
+                git branch -D "$branch"
+                echo "Deleted branch $branch"
+            case q Q
+                echo "Exiting."
+                return 0
+            case '*'
+                echo "Skipping branch $branch"
+        end
+    end
+end
+
 # Exports
 set --export --global HISTSIZE 10000
 set --export --global HISTFILESIZE 10000
