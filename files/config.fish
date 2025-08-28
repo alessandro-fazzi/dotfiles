0e8b283c3de4c72de8814336c768e09cafd7a9fc
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2025-08-28 16:32:05 +0200

    Experimenting claude code

--- files/config.fish
+++ files/config.fish
@@ -100,3 +100,6 @@ devbox global shellenv --init-hook | source
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init.fish 2>/dev/null || :
+
+
+alias claude="/Users/fuzzy/.claude/local/claude"
