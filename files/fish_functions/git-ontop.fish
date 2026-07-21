d786b67f80fe38a8d4364f3b7e6926291c308683
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2026-07-22 00:09:07 +0200

    Support stacked branches in git-ontop, skipping pull when parent has no upstream

--- files/fish_functions/git-ontop.fish
+++ files/fish_functions/git-ontop.fish
@@ -1,16 +1,20 @@
-function git-ontop -d "Update the default branch and rebase the current branch on top of it"
+function git-ontop -d "Update the parent branch (if it has a remote) and rebase the current branch on top of it"
     set -l remote origin
     if set -q argv[1]
         set remote $argv[1]
     end
 
-    set -l default (git symbolic-ref refs/remotes/$remote/HEAD | sed "s@^refs/remotes/$remote/@@")
+    set -l parent (git-parent-branch $remote)
     set -l current (git symbolic-ref --short HEAD)
 
-    echo (set_color green)"♻️ Updating $default and rebasing $current on top of it..."(set_color normal)
-
-    git switch "$default" \
-        && git pull "$remote" \
-        && git switch "$current" \
-        && git rebase "$default"
+    if git rev-parse --abbrev-ref --symbolic-full-name "$parent@{upstream}" >/dev/null 2>&1
+        echo (set_color green)"♻️ Updating $parent and rebasing $current on top of it..."(set_color normal)
+        git switch "$parent" \
+            && git pull "$remote" \
+            && git switch "$current" \
+            && git rebase "$parent"
+    else
+        echo (set_color green)"♻️ Rebasing $current on top of $parent (no upstream to pull)..."(set_color normal)
+        git rebase "$parent"
+    end
 end
